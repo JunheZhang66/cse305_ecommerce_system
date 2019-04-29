2c1ef23bb78679120004986cfc380263d1043ea1
Junhe Zhang <h3u8y4n6@gmail.com>
2019-04-29 14:42:06 -0700

    0.4

--- twittor/config.py
+++ twittor/config.py
@@ -2,3 +2,4 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:c9m2s2K2@localhost:3306/test"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'abc123'
--- twittor/models.py
+++ twittor/models.py
@@ -1,7 +1,8 @@
-from twittor import db
+from twittor import db, login_manager
 from datetime import datetime
-
-class User(db.Model):
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), unique=True, index=True)
@@ -12,6 +13,15 @@ class User(db.Model):
         return 'id={}, username={}, email={}, password_hash={}'.format(
             self.id,self.username,self.email,self.password_hash
         )
+
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(self.password_hash, password)
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 class Tweet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
