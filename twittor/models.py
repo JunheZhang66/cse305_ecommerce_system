0c8a980ee129b5e5c9751a7dde61f3b217fc08b4
Junhe Zhang <h3u8y4n6@gmail.com>
2019-05-01 15:36:21 -0700

    add user profile and followers

--- twittor/models.py
+++ twittor/models.py
@@ -1,13 +1,28 @@
 from twittor import db, login_manager
 from datetime import datetime
+from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     email = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(120), default="No information yet!")
+    create_time = db.Column(db.DateTime, default=datetime.utcnow)
     tweets = db.relationship('Tweet', backref='author',lazy='dynamic')
+
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return 'id={}, username={}, email={}, password_hash={}'.format(
@@ -19,6 +34,21 @@ class User(UserMixin,db.Model):
 
     def check_password(self,password):
         return check_password_hash(self.password_hash, password)
+    def avatar(self, size=80):
+        md5_digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            md5_digest, size
+        )
+    def follow(self,user):
+        if not self.is_following(user):
+            self.followed.append(user)
+    def unfollow(self,user):
+        if self.is_following(user):
+            self.followed.remove(user)
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() >0
+        
 @login_manager.user_loader
 def load_user(id):
     return User.query.get(int(id))
---
+++ twittor/readme.txt
@@ -0,0 +1,6 @@
+profile picture design:
+Gravatar:
+from hashlib import md5
+a=md5('junhe@gmail.com'.encode('utf-8')).hexdigest()
+
+http://www.gravatar.com/avatar/a
